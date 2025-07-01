b4e3207fee23314554ec4ad525abea92c97898cf
Kaique6 <kaiquegmadu@gmail.com>
2025-07-01 14:14:09 -0300

    pastas

--- server/express.js
+++ server/express.js
@@ -9,9 +9,18 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = 4000;
 
-//Data stuff
+//Data & image stuff
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')))
 app.use("/api/data", path.join(__dirname, '../data'))
 app.use("/api/data/images", path.join(__dirname, '../data/images'))
+app.use("/api/data/pfp", path.join(__dirname, '../data/pfp'))
 
 //Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main/index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log("Server running on port: " + port)
+})
