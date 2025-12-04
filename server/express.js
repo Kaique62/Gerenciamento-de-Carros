8a0e31d53c47d0399b7654bffb527abcc1eaf811
Kaique62 <81991014+Kaique62@users.noreply.github.com>
2025-12-04 16:52:04 -0300

    Update express.js

--- server/express.js
+++ server/express.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cookieParser());
